6540ae515a1dd879d9ac292be34e8ed34893514b
youngricky017-debug <young.ricky017@gmail.com>
2026-07-05 12:03:53 +0100

    Add Venue.tsx

---
+++ Venue.tsx
@@ -0,0 +1,10 @@
+export default function Venue(){
+  return (
+    <section style={{padding:40}}>
+      <h2>Venue Details</h2>
+      <div>Traditional Marriage - Kano</div>
+      <div>Church Wedding - Kano</div>
+      <div>Reception - Meena Event Center</div>
+    </section>
+  )
+}
